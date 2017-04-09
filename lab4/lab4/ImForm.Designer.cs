092b78d7e5eb498eb9f29bdadf2b717a04ed6bb0
MariaSavchuk <maccoriacco@gmail.com>
2017-04-09 20:23:09 +0200

    Lab 4 added

---
+++ lab4/lab4/ImForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace lab4
+{
+    partial class ImForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImageTable = new System.Windows.Forms.DataGridView();
+            this.openButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressLabel = new System.Windows.Forms.Label();
+            this.pictureView = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImageTable
+            // 
+            this.ImageTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ImageTable.Location = new System.Drawing.Point(32, 12);
+            this.ImageTable.Name = "ImageTable";
+            this.ImageTable.Size = new System.Drawing.Size(620, 220);
+            this.ImageTable.TabIndex = 0;
+            this.ImageTable.SelectionChanged += new System.EventHandler(this.ImageTable_SelectionChanged);
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(32, 251);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(142, 23);
+            this.openButton.TabIndex = 1;
+            this.openButton.Text = "Open files";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(210, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clear files";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.AutoSize = true;
+            this.progressLabel.Location = new System.Drawing.Point(51, 313);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(42, 13);
+            this.progressLabel.TabIndex = 3;
+            this.progressLabel.Text = "No files";
+            // 
+            // pictureView
+            // 
+            this.pictureView.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureView.Location = new System.Drawing.Point(366, 250);
+            this.pictureView.Name = "pictureView";
+            this.pictureView.Size = new System.Drawing.Size(333, 230);
+            this.pictureView.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureView.TabIndex = 4;
+            this.pictureView.TabStop = false;
+            // 
+            // ImForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 492);
+            this.Controls.Add(this.pictureView);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.ImageTable);
+            this.Name = "ImForm";
+            this.Text = "ImForm";
+            ((System.ComponentModel.ISupportInitialize)(this.ImageTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ImageTable;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label progressLabel;
+        private System.Windows.Forms.PictureBox pictureView;
+    }
+}
+
---
+++ lab4/lab4/ImForm.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace lab4
+{
+    public partial class ImForm : Form
+    {
+        public class Tuple<K, T>
+        {
+            public K Item1 { get; set; }
+            public T Item2 { get; set; }
+
+        }
+        public class ImageRow
+        {
+            public int Index { get; set; }
+            public string FileName { get; set; }
+            public String Size { get; set; }
+            public string Resolution { get; set; }
+            public string Compression { get; set; }
+            public ImageRow(int idx, Image image, string fileName)
+            {
+                Index = idx;
+                if (image != null)
+                {
+                    FileName = fileName;
+                    Size = image.Width + " x "+image.Height;
+                    Resolution = image.HorizontalResolution + " x " + image.VerticalResolution;
+                    Compression = GetCompressionType(image);
+                }
+
+            }
+
+            private static string GetCompressionType(Image image)
+            {
+                int compressionIdx = Array.IndexOf(image.PropertyIdList, 0x0103);
+                int compressionType = 0;
+                string compression = "Unknown compression";
+                if (compressionIdx != -1)
+                {
+                    PropertyItem compressionTag = image.PropertyItems[compressionIdx];
+                    compressionType = BitConverter.ToInt16(compressionTag.Value, 0);
+                }
+                else
+                {
+                    compression = "No compression";
+                }
+                switch (compressionType)
+                {
+                    case 1:
+                        compression = "No compression";
+                        break;
+                    case 2:
+                        compression = "CCITT Huffman";
+                        break;
+                    case 3:
+                        compression = "CCITT Group 3";
+                        break;
+                    case 4:
+                        compression = "CCITT Group 4";
+                        break;
+                    case 5:
+                        compression = "LZW";
+                        break;
+                    case 6:
+                        compression = "Old style JPEG";
+                        break;
+                    case 7:
+                        compression = "New style JPEG";
+                        break;
+                    case 8:
+                        compression = "Flate";
+                        break;
+                }
+                return compression;
+            }
+        }
+        private BackgroundWorker bWorker;
+        private BindingList<ImageRow> rows;
+        private List<Image> rowsImg;
+        public ImForm()
+        {
+            InitializeComponent();
+            rows = new BindingList<ImageRow>();
+            rowsImg = new List<Image>();
+            bWorker = new BackgroundWorker();
+            bWorker.DoWork += new DoWorkEventHandler(ProcessImages);
+            bWorker.ProgressChanged += new ProgressChangedEventHandler
+                    (PublishProgress);
+            bWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler
+                    (ProcessingCompleted);
+            bWorker.WorkerReportsProgress = true;
+            this.ImageTable.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            this.ImageTable.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            this.ImageTable.MultiSelect = false;
+            this.ImageTable.ReadOnly = true;
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "JPG|*.jpg;*.jpeg|PNG|*.png|BMP|*.bmp|GIF|*.gif|"
+                + "All Graphics Types|*.bmp;*.jpg;*.jpeg;*.png;*.gif;*.tif";
+            openFileDialog.FilterIndex = 5;
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.Multiselect = true;
+
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    this.openButton.Enabled = false;
+                    this.button2.Enabled = false;
+                    bWorker.RunWorkerAsync(openFileDialog);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void ProcessImages(object sender, DoWorkEventArgs e)
+        {
+            OpenFileDialog openFileDialog = (OpenFileDialog)e.Argument;
+            int filesProcessed = 0;
+
+            List<Tuple<ImageRow, Image>> imRows = new List<Tuple<ImageRow, Image>>();
+            double allLength = openFileDialog.FileNames.Length;
+            foreach (string filename in openFileDialog.FileNames)
+            {
+                filesProcessed++;
+                int progress = (int)((filesProcessed / allLength)*100);
+                bWorker.ReportProgress(progress);
+                Image image = Image.FromFile(filename);
+                ImageRow row = new ImageRow(rows.Count + filesProcessed, image, filename);
+                Tuple<ImageRow, Image> t = new Tuple<ImageRow, Image>();
+                t.Item1 = row;
+                t.Item2 = image;
+                imRows.Add(t);
+            }
+            e.Result = imRows;
+        }
+
+        private void PublishProgress(object sender, ProgressChangedEventArgs e)
+        {
+            progressLabel.Text = "Processing......" + e.ProgressPercentage +"%";
+        }
+
+        private void ProcessingCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            List<Tuple<ImageRow, Image>> imRows = (List<Tuple<ImageRow, Image>>)e.Result;
+            foreach (var im in imRows)
+            {
+                rows.Add(im.Item1);
+                rowsImg.Add(im.Item2);
+            }
+            progressLabel.Text = "Processing finished";
+            var source = new BindingSource(rows, null);
+            this.ImageTable.DataSource = source;
+            this.openButton.Enabled = true;
+            this.button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rows.Clear();
+            rowsImg.Clear();
+            this.pictureView.Image = null;
+        }
+
+        private void ImageTable_SelectionChanged(object sender, EventArgs e)
+        {
+            if (ImageTable.SelectedRows.Count > 0 && this.rowsImg.Count > 0)
+            {
+                //this.progressLabel.Text = "Row selected " + ImageTable.SelectedRows[0].Index.ToString();
+                this.pictureView.Image = this.rowsImg[ImageTable.SelectedRows[0].Index];
+            }
+        }
+    }
+}
